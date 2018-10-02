0bd8acca4b5f92f6c8be44aeef4c0403ba1d4d4b
DzianisKavalionak <nereidy4@gmail.com>
2018-10-02 12:45:41 +0300

    16 fail

--- src/index.js
+++ src/index.js
@@ -1,26 +1,68 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.arr = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this.arr.push('+', number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.arr.push('-', number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.arr.push('*', number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.arr.push('/', number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.arr.push('pow', number);
+    return this;
+  }
+
+
+    calculateNumbers() {
+      for(let i = 0; i < this.arr.length; i++){
+          if (this.arr[i] === 'pow'){
+              this.arr.splice(i - 1, 3, Math.pow(this.arr[i -1], this.arr[i + 1]));
+              this.calculateNumbers();
+          }
+      }
+      for(let i = 0; i < this.arr.length; i++){
+          if (this.arr[i] === '*' || this.arr[i] === '/'){
+              if (this.arr[i] === '*'){
+                  this.arr.splice(i - 1, 3, this.arr[i - 1] * this.arr[i + 1]);
+                  this.calculateNumbers();
+              } else {
+                  this.arr.splice(i - 1, 3, this.arr[i - 1] / this.arr[i + 1]);
+                  this.calculateNumbers();
+              }
+          }
+      }
+      for(let i = 0; i < this.arr.length; i++){
+          if (this.arr[i] === '+' || this.arr[i] === '-'){
+              if (this.arr[i] === '+'){
+                  this.arr.splice(i - 1, 3, this.arr[i - 1] + this.arr[i + 1]);
+                  this.calculateNumbers();
+              } else {
+                  this.arr.splice(i - 1, 3, this.arr[i - 1] - this.arr[i + 1]);
+                  this.calculateNumbers();
+              }
+          }
+      }
+      return this.arr[0];
+  }
+
+    valueOf() {
+    return this.calculateNumbers();
   }
 }
 
